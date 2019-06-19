21af2796036899ed07b00e121fb88843941ffae9
ArkAngels <kris.saputra@binus.ac.id>
2019-06-19 15:43:42 +0700

    test preloading

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
   <head>
     <meta charset="utf-8">
     <title>Loading...</title>
-    <script src="loading.js" charset="utf-8"></script>
-    <link rel="stylesheet" href="loading.css">
+    <script src="https://blog.xarkangels.com/loading.js" charset="utf-8"></script>
+    <link rel="stylesheet" href="https://blog.xarkangels.com/loading.css">
   </head>
   <body>
     <div id="loading">ArkAngels</div>
